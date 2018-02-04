321fe8f6c0e5d02d44b244efaa445d2e838d16bf
Paul Lai <plai@pl.com>
2018-02-04 16:03:01 -0500

    step 6 - final

--- mine-storomapath/src/main/java/com/tutorial/EmployeeRepository.java
+++ mine-storomapath/src/main/java/com/tutorial/EmployeeRepository.java
@@ -2,4 +2,9 @@ package com.tutorial;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {}
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+//    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+//    @Override
+//    void delete(Long aLong);
+}
